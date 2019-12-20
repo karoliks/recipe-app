c02719d0e780cde22416034110b2026d0bf59e8a
karoliks <karoline.k.sabo@gmail.com>
2019-12-20 18:29:22 +0100

    Remove side effects to get more functional code

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Recipe from "./Recipe";
 import "./App.css";
 import Burger from "./BurgerForRescipeSearch.svg";
 
-const App = () => {
-  const APP_ID = "2832d27a";
-  const APP_KEY = "dda7e49d2eecdfec712f65eb966f73b2";
+const APP_ID = "2832d27a";
+const APP_KEY = "dda7e49d2eecdfec712f65eb966f73b2";
 
+const getRecipes = async query => {
+  const response = await fetch(
+    `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+  );
+  return response.json();
+};
+
+const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("chickpea");
 
   useEffect(() => {
-    getRecipes();
+    getRecipes(query).then(data => {
+      setRecipes(data.hits);
+    });
   }, [query]);
-
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  };
 
   const updateSearch = e => {
     setSearch(e.target.value);
