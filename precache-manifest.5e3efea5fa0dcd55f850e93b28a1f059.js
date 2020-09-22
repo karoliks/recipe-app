c5052e943f1920c5381e235170e9358c233d17a1
karoliks <karoline.k.sabo@gmail.com>
2020-09-22 20:42:24 +0200

    Updates

---
+++ precache-manifest.5e3efea5fa0dcd55f850e93b28a1f059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9600516e07f58d1b971a6e556ca790d6",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "fde754151afdb07a3d72",
+    "url": "/recipe-app/static/css/main.75c77ed8.chunk.css"
+  },
+  {
+    "revision": "1fc8f8025bb1b685001f",
+    "url": "/recipe-app/static/js/2.dee9080a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.dee9080a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fde754151afdb07a3d72",
+    "url": "/recipe-app/static/js/main.6e6fa0e7.chunk.js"
+  },
+  {
+    "revision": "37892da72573d354a09f",
+    "url": "/recipe-app/static/js/runtime-main.6eab00d4.js"
+  },
+  {
+    "revision": "9d669ac1da5f97724e5e7a66b23ad20d",
+    "url": "/recipe-app/static/media/BurgerForRescipeSearch.9d669ac1.svg"
+  }
+]);
